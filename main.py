eb901fcb429b8e3db561b33b9f5838df34e00542
Abdullah <afzalabdullah066@gmail.com>
2023-08-14 17:25:10 +0500

    Project

---
+++ main.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, render_template
+
+app =Flask(__name__)
+
+@app.route("/")
+def home():
+
+    return  render_template('index.html')
+
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+@app.route("/detail")
+def dashboard():
+    return render_template('detail.html')
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/cart")
+def cart():
+    return render_template('cart.html')
+@app.route("/checkout")
+def checkout():
+    return render_template('checkout.html')
+
+app.run(debug=True)
+
